d405615160e54824d7a9da81ec8e3f1063733d1c
IJTINK7 <ijtink7@mail.ru>
2024-01-04 16:09:48 +0300

    Changing "CounterWithSettings" component

--- src/Counter/CounterWithSettings.tsx
+++ src/Counter/CounterWithSettings.tsx
@@ -2,39 +2,32 @@ import React, {useState} from "react";
 import './CounterWithSettings.css';
 
 export const CounterWithSettings = () => {
-	const [initialNumber, setNewNumber] = useState(0);
-	const [disableIncreaseButtonStatus, setDisableIncreaseButtonStatus] = useState(false)
-	const [disableResetButtonStatus, setDisableResetButtonStatus] = useState(true)
-	const increaseNumberHandler = (displayNumber: number) => {
-		setNewNumber(++displayNumber);
-		if (displayNumber === 1) {
-			setDisableResetButtonStatus(false);
-		}
-		if (displayNumber === 10) {
-			setDisableIncreaseButtonStatus(true);
-		}
-	}
+	const [disableSetButtonStatus, setDisableSetButtonStatus] = useState(false)
 	const resetDisplayHandler = () => {
-		setNewNumber(0);
-		setDisableResetButtonStatus(true);
-		setDisableIncreaseButtonStatus(false);
+		setDisableSetButtonStatus(true);
 	}
-	const display = initialNumber === 10 ? "RedDisplayValue" : "";
+	const setValuesToLocalStorage = () => {
+		// LocalStorage
+
+	}
+	//const display = initialNumber === 10 ? "RedDisplayValue" : "";
 	return (
 		<div className={"Container"}>
-			<div className={`${display} Display`}>{initialNumber}</div>
+			<div className={"Display2"}>
+				<div>
+					max value:
+					<input type="number"/>
+				</div>
+				<div>
+					max value:
+					<input type="number"/>
+				</div>
+			</div>
 			<div className={"ButtonsContainer"}>
 				<button
-					className={"IncreaseButton"}
-					onClick={() => {
-						increaseNumberHandler(initialNumber)
-					}}
-					disabled={disableIncreaseButtonStatus}>inc
-				</button>
-				<button
-					className={"ResetButton"}
-					onClick={resetDisplayHandler}
-					disabled={disableResetButtonStatus}>reset
+					className={"SetButton"}
+					onClick={setValuesToLocalStorage}
+					disabled={disableSetButtonStatus}>set
 				</button>
 			</div>
 		</div>
