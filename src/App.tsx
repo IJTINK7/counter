647b038727a9a39e5eae17bd68f91c1266de3a90
IJTINK7 <ijtink7@mail.ru>
2024-01-04 15:56:42 +0300

    Adding "CounterWithSettings" component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import {Counter} from "./Counter/Counter";
+import {CounterWithSettings} from "./Counter/CounterWithSettings";
 
 function App() {
   return (
     <div className="App">
+     <CounterWithSettings/>
      <Counter/>
     </div>
   );
