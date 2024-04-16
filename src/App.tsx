0e935a9c9eb49abd057b925cee0cb307c75f5c89
IJTINK7 <ijtink7@mail.ru>
2024-04-16 13:25:31 +0300

    Commenting "CounterWithSettings" component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import {Counter} from "./Counter/Counter";
-import {CounterWithSettings} from "./Counter/CounterWithSettings";
 
 function App() {
   return (
     <div className="App">
-     <CounterWithSettings/>
+     {/*<CounterWithSettings/>*/}
      <Counter/>
     </div>
   );
